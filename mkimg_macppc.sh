4aa6dc369b272d9d23caac312b420bbbabf5ccd5
nia <nia@NetBSD.org>
2025-04-20 00:37:51 +0200

    macppc: to 10.1, reduce memory

--- mkimg_macppc.sh
+++ mkimg_macppc.sh
@@ -11,7 +11,7 @@
 # With the GENERIC kernel, the root device (wd0c) needs to be specified
 # by hand at the root device prompt once it's booted.
 #
-RELEASE="9.4"
+RELEASE="10.1"
 ARCH="macppc"
 SET_SUFFIX=".tgz"
 SETS="base comp etc games man misc modules text"
--- qemu_macppc.sh
+++ qemu_macppc.sh
@@ -1,16 +1,10 @@
 #!/bin/sh
 qemu-system-ppc -nographic \
-	-m 512 -machine mac99,via=pmu \
+	-m 256 -machine mac99,via=pmu \
 	-cdrom ./workdir/ofwboot.qcow2 \
-	-hda workdir/NetBSD-9.4-macppc.qcow2 \
+	-hda workdir/NetBSD-10.1-macppc.qcow2 \
 	-prom-env boot-device=cd:\ofwboot.xcf \
 	-prom-env boot-file=hd:/netbsd \
 	-netdev user,id=wm0 \
 	-device e1000,netdev=wm0 \
 	$*
-
-	# virtio at pci... testing
-	#-netdev user,id=vioif0 \
-	#-device virtio-net-pci,netdev=vioif0 \
-	#-object rng-random,filename=/dev/urandom,id=rng0 \
-	#-device virtio-rng-pci,rng=rng0 \
